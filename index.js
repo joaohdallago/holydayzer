bda2a36537176c8dbd518e80f26174cbc2136dc3
joaohdallago <joaohdallago@gmail.com>
2022-04-20 17:15:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import dayjs from 'dayjs';
+
+const app = express();
+
+const holidays = [
+    { date: "1/1/2022", name: "Confraternização mundial" },
+    { date: "1/3/2022", name: "Carnaval" },
+    { date: "4/17/2022", name: "Páscoa" },
+    { date: "4/21/2022", name: "Tiradentes" },
+    { date: "5/1/2022", name: "Dia do trabalho" },
+    { date: "6/16/2022", name: "Corpus Christi" },
+    { date: "9/7/2022", name: "Independência do Brasil" },
+    { date: "10/12/2022", name: "Nossa Senhora Aparecida" },
+    { date: "11/2/2022", name: "Finados" },
+    { date: "11/15/2022", name: "Proclamação da República" },
+    { date: "12/25/2022", name: "Natal" }
+];
+
+const today = dayjs().format('MM/DD/YYYY');
+
+const todaysHoliday = holidays.find(holiday => holiday.date === today);
+
+app.get('/holidays', (req, res) => {
+    res.send(holidays)
+});
+
+app.get('/is-today-holiday', (req, res) => {
+    const message = todaysHoliday ?
+        `Sim, hoje é ${todaysHoliday.name}`
+    :
+        'Não, hoje não é feriado'
+
+    res.send(message)
+})
+
+app.listen(4000, () => {
+    console.log('server ta on')
+})
